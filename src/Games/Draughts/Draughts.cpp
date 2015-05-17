3ee4b56c50398ad549ba33371cb7728cf2f4971a
Luggatariff <petit.anael@gmail.com>
2015-05-17 20:01:33 +0200

    Draughts added, unfinished (still Queen moves to implement in update_playable_moves)

---
+++ src/Games/Draughts/Draughts.cpp
@@ -0,0 +1,405 @@
+/*!
+ * \file Draughts.cpp
+ * \brief Draughts code
+ * \author Anaël Petit
+ */
+
+#include <Draughts/Draughts.hpp>
+#include <algorithm>
+
+Draughts::Draughts(Player * player_one, Player * player_two){
+	Coordinates board_size(2);
+	board_size[0] = DRAUGHTS_HEIGHT;
+	board_size[1] = DRAUGHTS_WIDTH;
+	this->t_board = new Board<Draughts_Attributes>(board_size);
+
+	this->t_players.push_back(player_one);
+	this->t_players.push_back(player_two);
+	this->t_next_player = player_one;
+
+	this->t_black_pawns = 0;
+	this->t_black_queens = 0;
+	this->t_black_total = 0;
+	this->t_white_pawns = 0;
+	this->t_white_queens = 0;
+	this->t_white_total = 0;
+
+	Coordinates square(2);
+	for (square[0]=0; square[0]<DRAUGHTS_HEIGHT; square[0]++){
+		for (square[1]=0; square[1]<DRAUGHTS_WIDTH; square[1]++){
+			if ((square[0] + square[1]) % 2 == 0 ){
+				t_board->getSquare(square)->addAttribute(DRAUGHTS_EMPTY);
+			}
+			else{
+				t_board->getSquare(square)->addAttribute(DRAUGHTS_UNPLAYABLE);
+			}
+		}
+	}
+}
+
+Draughts::~Draughts(){
+	delete t_board;
+}
+
+Game * Draughts::copy(){
+	Draughts * result = new Draughts(this->t_players[0], this->t_players[1]);
+	delete result->t_board;
+
+	result->t_board = this->t_board->copy();
+	result->t_next_player = this->t_next_player;
+	result->t_playable_moves = this->t_playable_moves;
+	result->t_black_pawns = this->t_black_pawns;
+	result->t_black_queens = this->t_black_queens;
+	result->t_black_total = this->t_black_total;
+	result->t_white_pawns = this->t_white_pawns;
+	result->t_white_queens = this->t_white_queens;
+	result->t_white_total = this->t_white_total;
+
+	return (Game *)result;
+}
+
+vector<Player *> Draughts::players(){
+	return t_players;
+}
+
+bool Draughts::isEnded(){
+	if (isWon()) return true;
+	return false;
+}
+bool Draughts::isWon(){
+	return (whoWon() != -1);
+}
+int Draughts::whoWon(){
+	if (isWinner(t_players[0]->getTeam())) return t_players[0]->getTeam();
+	if (isWinner(t_players[1]->getTeam())) return t_players[1]->getTeam();
+	return -1;
+}
+bool Draughts::isWinner(unsigned int team_id){
+	bool result = false;
+
+	if (team_id == t_players[0]->getTeam()){
+		result = (this->t_black_total == 0 || (this->t_next_player == t_players[1] && this->t_playable_moves.empty()));
+	}
+	else{
+		result = (this->t_white_total == 0 || (this->t_next_player == t_players[0] && this->t_playable_moves.empty()));
+	}
+
+	return result;
+}
+
+int Draughts::score(unsigned int team_id){
+	int winner = whoWon();
+	if (winner == (int)team_id) return victoryScore();
+	if (winner == -1) return 0;
+	return -victoryScore();
+}
+int Draughts::victoryScore(){
+	return DRAUGHTS_MAX_SCORE;
+}
+
+bool Draughts::check_height(int height){
+	return (height >=0 && height < DRAUGHTS_HEIGHT);
+}
+
+bool Draughts::check_width(int width){
+	return (width >= 0 && width < DRAUGHTS_WIDTH);
+}
+
+bool Draughts::check_existence(Coordinates square){
+	return check_height(square[0]) && check_width(square[1]);
+}
+
+void Draughts::update_playable_moves(bool change_player){
+	t_playable_moves.clear();
+
+	bool player_changed=false;
+
+	// It's necessary to discriminate taking moves from simple moves (taking moves have to be played)
+	vector<Coordinates> simple_moves;
+	vector<Coordinates> taking_moves;
+
+	do{
+		simple_moves.clear();
+		taking_moves.clear();
+
+		//White pawns move to the up of the board
+		int move_direction = 1;
+		Draughts_Attributes player_pawn = DRAUGHTS_WHITE_PAWN;
+		Draughts_Attributes player_queen = DRAUGHTS_WHITE_QUEEN;
+		Draughts_Attributes adverse_pawn = DRAUGHTS_BLACK_PAWN;
+		Draughts_Attributes adverse_queen = DRAUGHTS_BLACK_QUEEN;
+		if (t_next_player == t_players[1]){
+			//Black pawns move backward
+			move_direction = -1;
+			player_pawn = DRAUGHTS_BLACK_PAWN;
+			player_queen = DRAUGHTS_BLACK_QUEEN;
+			adverse_pawn = DRAUGHTS_WHITE_PAWN;
+			adverse_queen = DRAUGHTS_WHITE_QUEEN;
+		}
+
+		Coordinates square(2);
+		for (square[0]=0; square[0]<DRAUGHTS_HEIGHT; square[0]++){
+			for (square[1]=0; square[1]<DRAUGHTS_WIDTH; square[1]++){
+				Square<Draughts_Attributes> * board_square = t_board->getSquare(square);
+				// First, check if the square represents a playable piece
+				if (! (board_square->isAttribute(DRAUGHTS_UNPLAYABLE) || board_square->isAttribute(DRAUGHTS_EMPTY))){
+					// Then it could be the source of a move
+					Coordinates potential_move(4);
+					potential_move[0] = square[0];
+					potential_move[1] = square[1];
+
+					// differentiate a pawn from a queen
+					if (board_square->isAttribute(player_pawn)){
+						for (int height_direction = -1; height_direction <= 1; height_direction += 2){
+							for (int width_direction = -1; width_direction <= 1; width_direction += 2){
+								if (check_height(square[0] + 2*height_direction)){
+									if (check_width(square[1] + 2*width_direction)){
+										Coordinates potential_destination(2);
+										potential_destination[0] = square[0] + 2*height_direction;
+										potential_destination[1] = square[1] + 2*width_direction;
+										if (t_board->getSquare(potential_destination)->isAttribute(DRAUGHTS_EMPTY)){
+											Coordinates potential_take(2);
+											potential_take[0] = square[0] + height_direction;
+											potential_take[1] = square[1] + width_direction;
+											if (t_board->getSquare(potential_take)->isAttribute(adverse_pawn) || t_board->getSquare(potential_take)->isAttribute(adverse_queen)){
+												potential_move[2] = potential_destination[0];
+												potential_move[3] = potential_destination[1];
+												taking_moves.push_back(potential_move);
+											}
+										}
+									}
+								}
+							}
+						}
+
+						//if there is already taking moves, simple_moves are no longer needed to compute
+						if (taking_moves.empty()){
+							int height_direction = move_direction;
+							for (int width_direction = -1; width_direction <= 1; width_direction += 2){
+								if (check_height(square[0] + height_direction)){
+									if (check_width(square[1] + width_direction)){
+										Coordinates potential_destination(2);
+										potential_destination[0] = square[0] + height_direction;
+										potential_destination[1] = square[1] + width_direction;
+										if (t_board->getSquare(potential_destination)->isAttribute(DRAUGHTS_EMPTY)){
+											potential_move[2] = potential_destination[0];
+											potential_move[3] = potential_destination[1];
+											simple_moves.push_back(potential_move);
+										}
+									}
+								}
+							}
+						}
+					}
+					else if (board_square->isAttribute(player_queen)){
+
+
+						//if there is already taking moves, simple_moves are no longer needed to compute
+						if (taking_moves.empty()){
+
+						}
+					}
+				}
+			}
+		}
+		if (change_player && taking_moves.empty()){
+			if (t_next_player == t_players[1]){
+				t_next_player = t_players[0];
+			}
+			else{
+				t_next_player = t_players[1];
+			}
+			player_changed = true;
+		}
+		else
+			player_changed = false;
+	}
+	while (player_changed);
+
+	if (!taking_moves.empty()){
+		t_playable_moves=taking_moves;
+	}
+	else{
+		t_playable_moves=simple_moves;
+	}
+}
+
+void Draughts::start(){
+	t_last_moves.clear();
+
+	Coordinates square(2);
+	for (square[0]=0; square[0]<DRAUGHTS_HEIGHT/2-1; square[0]++){
+		for (square[1]=0; square[1]<DRAUGHTS_WIDTH; square[1]++){
+			if (t_board->getSquare(square)->isAttribute(DRAUGHTS_EMPTY)){
+				t_board->getSquare(square)->delAttribute(DRAUGHTS_EMPTY);
+				t_board->getSquare(square)->addAttribute(DRAUGHTS_WHITE_PAWN);
+				this->t_white_pawns++;
+			}
+		}
+	}
+	for (square[0]=DRAUGHTS_HEIGHT/2+1; square[0]<DRAUGHTS_HEIGHT; square[0]++){
+		for (square[1]=0; square[1]<DRAUGHTS_WIDTH; square[1]++){
+			if (t_board->getSquare(square)->isAttribute(DRAUGHTS_EMPTY)){
+				t_board->getSquare(square)->delAttribute(DRAUGHTS_EMPTY);
+				t_board->getSquare(square)->addAttribute(DRAUGHTS_BLACK_PAWN);
+				this->t_black_pawns++;
+			}
+		}
+	}
+
+	this->t_black_total = this->t_black_pawns;
+	this->t_white_total = this->t_white_pawns;
+	this->t_black_queens = 0;
+	this->t_white_queens = 0;
+	this->t_next_player = t_players[0];
+
+	update_playable_moves(false);
+}
+unsigned int Draughts::dimension(){
+	return 4;
+}
+
+bool Draughts::isPlayable(Coordinates coordinates){
+	if (isEnded()) return false;
+
+	return (find(this->t_playable_moves.begin(), this->t_playable_moves.end(), coordinates) != this->t_playable_moves.end());
+}
+vector<Coordinates> Draughts::playableCoordinates(){
+	return this->t_playable_moves;
+}
+
+Player * Draughts::nextPlayer(){
+	return t_next_player;
+}
+
+Draughts_Attributes Draughts::get_piece(Square<Draughts_Attributes> * draughts_square){
+	if (draughts_square->isAttribute(DRAUGHTS_BLACK_PAWN)){
+		return DRAUGHTS_BLACK_PAWN;
+	}
+	else if (draughts_square->isAttribute(DRAUGHTS_WHITE_PAWN)){
+		return DRAUGHTS_WHITE_PAWN;
+	}
+	else if (draughts_square->isAttribute(DRAUGHTS_WHITE_QUEEN)){
+		return DRAUGHTS_WHITE_QUEEN;
+	}
+	else{
+		return DRAUGHTS_BLACK_QUEEN;
+	}
+}
+
+void Draughts::set_to_empty(Square<Draughts_Attributes> * draughts_square){
+	if (draughts_square->isAttribute(DRAUGHTS_BLACK_PAWN)){
+		draughts_square->delAttribute(DRAUGHTS_BLACK_PAWN);
+		this->t_black_pawns--;
+		this->t_black_total--;
+	}
+	else if (draughts_square->isAttribute(DRAUGHTS_WHITE_PAWN)){
+		draughts_square->delAttribute(DRAUGHTS_WHITE_PAWN);
+		this->t_white_pawns--;
+		this->t_white_total--;
+	}
+	else if (draughts_square->isAttribute(DRAUGHTS_WHITE_QUEEN)){
+		draughts_square->delAttribute(DRAUGHTS_WHITE_QUEEN);
+		this->t_white_queens--;
+		this->t_white_total--;
+	}
+	else{
+		draughts_square->delAttribute(DRAUGHTS_BLACK_QUEEN);
+		this->t_black_queens--;
+		this->t_black_total--;
+	}
+	draughts_square->addAttribute(DRAUGHTS_EMPTY);
+}
+
+void Draughts::play(Coordinates coordinates){
+	if (isPlayable(coordinates)){
+		t_last_moves.push_back(coordinates);
+		Coordinates source(2);
+		Coordinates destination(2);
+		source[0] = coordinates[0];
+		source[1] = coordinates[1];
+		destination[0] = coordinates[2];
+		destination[1] = coordinates[3];
+
+		Draughts_Attributes source_piece = get_piece(t_board->getSquare(source));
+		t_board->getSquare(source)->delAttribute(source_piece);
+		t_board->getSquare(source)->addAttribute(DRAUGHTS_EMPTY);
+
+		Draughts_Attributes destination_piece = source_piece;
+		if ( source_piece == DRAUGHTS_WHITE_PAWN && destination[0] == DRAUGHTS_HEIGHT - 1){
+			destination_piece = DRAUGHTS_WHITE_QUEEN;
+			this->t_white_queens++;
+			this->t_white_pawns--;
+		}
+		else if ( source_piece == DRAUGHTS_BLACK_PAWN && destination[0] == 0){
+			destination_piece = DRAUGHTS_BLACK_QUEEN;
+			this->t_black_queens++;
+			this->t_black_pawns--;
+		}
+		t_board->getSquare(destination)->delAttribute(DRAUGHTS_EMPTY);
+		t_board->getSquare(destination)->addAttribute(destination_piece);
+
+		Coordinates minimal(2);
+		Coordinates maximal(2);
+		for (unsigned int i = 0; i<2; i++){
+			minimal[i] = (source[i] < destination[i]) ? source[i] : destination[i];
+			maximal[i] = (source[i] > destination[i]) ? source[i] : destination[i];
+		}
+
+		Coordinates square(2);
+		unsigned int offset = 1;
+		square[0] = minimal[0] + offset;
+		square[1] = minimal[1] + offset;
+		while (! (square == maximal)){
+			if (!t_board->getSquare(square)->isAttribute(DRAUGHTS_EMPTY)){
+				set_to_empty(t_board->getSquare(square));
+				break;
+			}
+			offset++;
+			square[0] = minimal[0] + offset;
+			square[1] = minimal[1] + offset;
+		}
+
+		update_playable_moves();
+	}
+}
+vector<Coordinates> Draughts::lastMoves(){
+	return this->t_last_moves;
+}
+
+void Draughts::display(std::ostream & out){
+	out<<" ";
+	for(int column=0; column<DRAUGHTS_WIDTH; column++)
+		out<<"|"<<column;
+	out<<"|"<<std::endl;
+	Coordinates coordinates(2);
+	for(int line = DRAUGHTS_HEIGHT - 1; line>=0; line--){
+		out<<" ";
+		for(coordinates[1]=0; coordinates[1]<DRAUGHTS_WIDTH; coordinates[1]++){
+			coordinates[0] = (unsigned int) line;
+
+			out<<"|";
+			if (t_board->getSquare(coordinates)->isAttribute(DRAUGHTS_WHITE_PAWN))
+				out<<'o';
+			else if (t_board->getSquare(coordinates)->isAttribute(DRAUGHTS_BLACK_PAWN))
+				out<<'x';
+			else if (t_board->getSquare(coordinates)->isAttribute(DRAUGHTS_BLACK_QUEEN))
+				out<<'X';
+			else if (t_board->getSquare(coordinates)->isAttribute(DRAUGHTS_WHITE_QUEEN))
+				out<<'O';
+			else
+				out<<" ";
+		}
+		out<<"|"<<std::endl;
+	}
+	out<<" ";
+	for(int column=0; column<DRAUGHTS_WIDTH; column++)
+		out<<"|-";
+	out<<"|"<<std::endl;
+}
+
+string Draughts::getName(){
+	return string("Draughts");
+}
+
+
---
+++ src/Games/Draughts/Draughts.hpp
@@ -0,0 +1,116 @@
+/*!
+ * \file Draughts.hpp
+ * \brief Draughts description
+ * \author Anaël Petit
+ */
+
+#ifndef DRAUGHTS_HPP
+#define DRAUGHTS_HPP
+
+#define DRAUGHTS_WIDTH 	10		/*!<Draughts board width*/
+#define DRAUGHTS_HEIGHT	10		/*!<Draughts board height*/
+#define DRAUGHTS_MAX_SCORE 50	/*!<Draughts Maximal score (victory)*/
+
+#include "Game.hpp"
+#include "Board.hpp"
+
+/*!
+ * \brief Attributes than can be found in Draughts Board Squares
+ */
+enum Draughts_Attributes{
+	DRAUGHTS_UNPLAYABLE,
+	DRAUGHTS_EMPTY,
+	DRAUGHTS_BLACK_PAWN,
+	DRAUGHTS_WHITE_PAWN,
+	DRAUGHTS_BLACK_QUEEN,
+	DRAUGHTS_WHITE_QUEEN
+};
+
+/*!
+ * \class Draughts
+ * \brief class representing a Draughts Game
+ */
+class Draughts: public Game{
+	Board<Draughts_Attributes> * t_board;	/*!<Draughts Game Board*/
+	vector<Player *> t_players;				/*!<Draughts Players */
+	Player * t_next_player;					/*!<next Player*/
+	vector<Coordinates> t_last_moves;		/*!<last moves played*/
+
+	vector<Coordinates> t_playable_moves;	/*!<playable moves*/
+	unsigned int t_white_pawns;				/*!<Number of white pawns*/
+	unsigned int t_black_pawns;				/*!<Number of black pawns*/
+	unsigned int t_white_queens;			/*!<Number of white queens*/
+	unsigned int t_black_queens;			/*!<Number of black queens*/
+	unsigned int t_white_total;				/*!<White total number (pawns+queens)*/
+	unsigned int t_black_total;				/*!<Black total number (pawns+queens)*/
+
+	/*!
+	 * \brief sets a square with unknown but certain content to empty
+	 * \param draughts_square : square to set to empty
+	 */
+	void set_to_empty(Square<Draughts_Attributes> * draughts_square);
+
+	/*!
+	 * \brief gets the content of a non empty Draughts square
+	 * \param draughts_square : non empty square
+	 */
+	Draughts_Attributes get_piece(Square<Draughts_Attributes> * draughts_square);
+
+	/*!
+	 * \brief updates t_playable_moves attribute depending on the board structure
+	 * \param change_player : indicates if the player has to change if there is no pawn to take
+	 */
+	void update_playable_moves(bool change_player=true);
+
+	/*!
+	 * \brief checks if the given height is in the board
+	 * \param height : given height
+	 * \return true if the height is in the board
+	 */
+	inline bool check_height(int height);
+	/*!
+	 * \brief checks if the given width is in the board
+	 * \param width : given width
+	 * \return true if the width is in the board
+	 */
+	inline bool check_width(int width);
+	/*!
+	 * \brief checks if the given square is in the board
+	 * \param square : square Coordinates
+	 * \return true if the square is in the board
+	 */
+	inline bool check_existence(Coordinates square);
+public:
+	/*!
+	 * \brief Draughts constructor
+	 * \param player_one : First Player
+	 * \param player_two : Second Player
+	 */
+	Draughts(Player * player_one, Player * player_two);
+	~Draughts();
+	Game * copy();
+
+	vector<Player *> players();
+
+	bool isEnded();
+	bool isWon();
+	int whoWon();
+	bool isWinner(unsigned int team_id);
+
+	int score(unsigned int team_id);
+	int victoryScore();
+
+	void start();
+	unsigned int dimension();
+	bool isPlayable(Coordinates coordinates);
+	vector<Coordinates> playableCoordinates();
+
+	Player * nextPlayer();
+	void play(Coordinates coordinates);
+	vector<Coordinates> lastMoves();
+
+	void display(std::ostream & out);
+	string getName();
+};
+
+#endif
